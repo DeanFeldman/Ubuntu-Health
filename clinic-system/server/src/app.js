c3f0fb688313c7de4bca89ef249259e7d1c4a1c0
Dean Feldman <dean.feldman05@gmail.com>
2026-04-05 20:04:25 +0200

    Update app.js

--- clinic-system/server/src/app.js
+++ clinic-system/server/src/app.js
@@ -69,7 +69,7 @@ app.get('/api/clinics/:id', async (req, res) => {
 })
 
 // Serve React frontend
-const publicPath = path.join(__dirname, 'public')
+const publicPath = path.join(__dirname, '..', 'public')
 app.use(express.static(publicPath))
 
 app.get('*', (req, res) => {
