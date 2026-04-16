0f0f4273076b7f5d6341a0afde1aab80aee66b2d
Shayna Unterslak <shaynaunterslak@gmail.com>
2026-04-16 21:12:26 +0200

    test: add QueuePage tests and user-event dependency

--- clinic-system/client/src/main.jsx
+++ clinic-system/client/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './app'
 import { AuthProvider } from './context/AuthContext'
-
+window.__API_BASE__ = import.meta.env.VITE_API_BASE_URL || ''
 {/*Create root and render the entire React app */}
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
