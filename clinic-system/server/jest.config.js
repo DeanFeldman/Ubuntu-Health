97830d1ec10c80df503b7426cb9e91af0c7d57dc
Shayna Unterslak <shaynaunterslak@gmail.com>
2026-04-17 15:27:38 +0200

    test: configure full frontend and backend coverage reporting

--- clinic-system/server/jest.config.js
+++ clinic-system/server/jest.config.js
@@ -1,8 +1,21 @@
 module.exports = {
   testEnvironment: 'node',
+
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+
+  collectCoverage: true,
+
   coverageDirectory: 'coverage',
+
   collectCoverageFrom: [
     'src/**/*.js',
+
+    '!src/**/*.test.js',
+    '!src/tests/**',
+    '!src/**/__tests__/**',
+
+    '!src/index.js'
   ],
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+
+  coverageReporters: ['text', 'lcov', 'html']
 }
